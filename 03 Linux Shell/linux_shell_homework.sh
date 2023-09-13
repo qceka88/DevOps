6ba99e7b59c4b6ed8c8a01c0ada0eece5482cc0a
Yanko Rashkov <thewhitedead@abv.bg>
2023-09-13 20:28:19 +0300

    create 03 linux shell

---
+++ 03 Linux Shell/linux_shell_homework.sh
@@ -0,0 +1,17 @@
+$ pwd : return where is our current location
+$ ls : return files and folders in our current location
+$ ls -al: returnfiles and folders with long listing formats in our current location
+$ ls -a: do not ignore entries starting with .
+$ cd /dir: open directory
+$ cd (no mentioned dir): return to main location
+$ ls os*: when we want to address all files which name starts with something. use "startswith" "*"
+$ cat os-release: when we want to check what file contains use "cat" "filename"
+$ hostname: return name of the host
+$ cat /etc/hostname: returns name of the host trough dir
+$ logout: logout the user
+$ date: return date
+$ date +%Y-%m-%d: return date in format year: month: day
+$ cal: return calendar for current month
+$ cal -(number):  returns calendar for month backward with the number
+$ uptime: returns how long our system is runing
+$ hystory: returns hystory of used commands
