50312a52a54861479df23ecf8e3643fd400e4e2a
Yanko Rashkov <thewhitedead@abv.bg>
2023-10-05 12:26:02 +0300

    add infrastructure as code homework

---
+++ Cloud-And-Containers/09-Infrastructure-as-Code/terminal_commands.sh
@@ -0,0 +1,12 @@
+#start with init to start the configuration
+terraform init
+# to format configurations
+terraform fmf
+# to  validate configurations
+terraform validate
+# to create infrastructure
+terraform apply
+#inspect current state
+terraform show
+#to destroy configuration. delete the container and image
+terraform destroy
