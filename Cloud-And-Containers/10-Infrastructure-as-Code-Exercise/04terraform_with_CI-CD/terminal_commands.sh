1e25906d0a4e8f0b2d68fbb7666e714b3d0f0850
Yanko Rashkov <thewhitedead@abv.bg>
2023-10-09 18:29:58 +0300

    Update terminal_commands.sh

--- Cloud-And-Containers/10-Infrastructure-as-Code-Exercise/04terraform_with_CI-CD/terminal_commands.sh
+++ Cloud-And-Containers/10-Infrastructure-as-Code-Exercise/04terraform_with_CI-CD/terminal_commands.sh
@@ -1,4 +1,4 @@
 az ad sp create-for-rbac --name "ATGHA-Yanko123456" --role contributor --scopes /subscriptions/"your subscription id from azure portal" --sdk-auth
 az group create --name StorrageTaskBoard-48395 --location westeurope
 az storage account create --name yrtaskboardstorage --resource-group StorrageTaskBoard-48395 --location westeurope --sku Standard_LRS --kind StorageV2
-az storage container create -n yrtaskboardcontainer --account-name yrtaskboardstorage
+az storage container create -n yrtaskboardcontainer --account-name yrtaskboardstorage  
