a52a24e40968a15b53cc8d3aa3eee36d236391fd
Yanko Rashkov - Senior <150540940+qceka88x1@users.noreply.github.com>
2023-11-11 21:50:57 +0200

    Added functionality to the button

--- Software-Engineering-and-DevOps/06-Exercise-Git-and-GitHub/01towns/towns.js
+++ Software-Engineering-and-DevOps/06-Exercise-Git-and-GitHub/01towns/towns.js
@@ -2,6 +2,11 @@ $(document).ready(function() {
 	$('#btnDelete').click(deleteTown)
 });
 
+$(document).ready(function() {
+	$('#btnAdd').click(addTown);
+});
+
+
 function deleteTown() {
 	let townName = $('#townName').val();
 	$('#townName').val('');
@@ -16,4 +21,11 @@ function deleteTown() {
 		$('#result').text(townName + " deleted.");
 	else
 		$('#result').text(townName + " not found.");
+}
+
+function addTown() {
+    let townName = $('#townNameForAdd').val();
+    $('#townNameForAdd').val('');
+    $('#towns').append($('<option>').text(townName));
+    $('#result').text(townName + " added.");
 }
