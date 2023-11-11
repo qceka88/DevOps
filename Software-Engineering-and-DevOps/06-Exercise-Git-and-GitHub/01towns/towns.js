73dcc4fca70f415b2de21ff1bf3879a183d7b5ba
Yanko Rashkov - Junior <150541527+qceka88x3@users.noreply.github.com>
2023-11-11 21:44:06 +0200

    Added functionality

--- Software-Engineering-and-DevOps/06-Exercise-Git-and-GitHub/01towns/towns.js
+++ Software-Engineering-and-DevOps/06-Exercise-Git-and-GitHub/01towns/towns.js
@@ -3,17 +3,24 @@ $(document).ready(function() {
 });
 
 function deleteTown() {
-	let townName = $('#townName').val();
-	$('#townName').val('');
-	let removed = false;
-	for (let option of $('#towns option')) {
-		if (option.textContent == townName) {
-			removed = true;
-			option.remove();
-		}
-	}
-	if (removed)
-		$('#result').text(townName + " deleted.");
-	else
-		$('#result').text(townName + " not found.");
+    let townName = $('#townName').val();
+    $('#townName').val('');
+    let removed = false;
+    for (let option of $('#towns option')) {
+        if (option.textContent == townName) {
+            removed = true;
+            option.remove();
+        }
+    }
+    if (removed)
+        showMessage(townName + " deleted.");
+    else
+        showMessage(townName + " not found.");
+}
+
+function showMessage(msg) {
+    $('#result').text(msg).css("display", "block");
+    setTimeout(function () {
+        $('#result').hide('blind', {}, 500);
+    }, 3000);
 }
