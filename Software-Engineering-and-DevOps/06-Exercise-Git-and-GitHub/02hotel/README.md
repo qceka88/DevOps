c02deffe8edb0a6625b1edd481d182d864fd0e04
Yanko Rashkov <thewhitedead@abv.bg>
2023-11-12 01:46:11 +0200

    update

---
+++ Software-Engineering-and-DevOps/06-Exercise-Git-and-GitHub/02hotel/README.md
@@ -0,0 +1,8 @@
+## Hotels
+# Exercise for students in the teamwork course at SoftUni-Svetlina @Software Engineering cource
+
+# Roles
+  [Yanko - Leader](https://github.com/qceka88) takes the role "Admin".<br>
+  [Yanko - Senior](https://github.com/qceka88x1) takes the role “Verifier”.<br>
+  [Yanko - Mid](https://github.com/qceka88x2) takes the role “Offerer”.<br>
+  [Yanko - Junior](https://github.com/qceka88x3) takes the role “Questioner”.
--- Software-Engineering-and-DevOps/06-Exercise-Git-and-GitHub/02hotel/solution.js
+++ Software-Engineering-and-DevOps/06-Exercise-Git-and-GitHub/02hotel/solution.js
@@ -21,3 +21,109 @@ document.querySelector('#new-reservation').addEventListener('click', (e) => clea
 function cleanData(e) {
     changeContent('search-form-content');
 }
+
+changeContent('search-form-content');
+document.querySelector('#search-form-button').addEventListener('click', (e) => searchFormData(e));
+
+function searchFormData(e) {
+    e.preventDefault();
+    const data = e.target.parentElement;
+    const checkIn = data.querySelector('#check-in').value;
+    const checkOut = data.querySelector('#check-out').value;
+    const people = data.querySelector('#people').value;
+    if (checkIn != '' && checkOut != '' && people != '' &&
+        new Date(checkIn) <= new Date(checkOut)) {
+        reservation.startDate = checkIn;
+        reservation.endDate = checkOut;
+        reservation.guestsCount = people;
+        console.log(reservation);
+        changeContent('search-result-form-content');
+    }
+}
+document.querySelector('#search-back-btn').addEventListener('click', (e) => fillSearchForm(e));
+
+function fillSearchForm(e) {
+    e.preventDefault();
+    changeContent('search-form-content');
+    document.querySelector('#check-in').value = reservation.startDate;
+    document.querySelector('#check-out').value = reservation.endDate;
+    document.querySelector('#people').value = reservation.guestsCount;
+}
+
+
+document.querySelectorAll('.room-type').forEach(room => {
+    room.addEventListener("click", (e) => selectRoomType(e))
+});
+
+function selectRoomType(e) {
+    let myTarget = undefined;
+    e.preventDefault;
+    if (e.target.querySelector('img') != null) {
+        myTarget = e.target;
+    } else {
+        myTarget = e.target.parentElement;
+    }
+    document.querySelectorAll('.room-type').forEach(room =>
+        room.classList.remove('selected-room'));
+    myTarget.classList.add('selected-room');
+}
+
+document.querySelector('#search-next-btn').addEventListener('click', (e) => findRoom(e));
+
+function findRoom(e) {
+    e.preventDefault();
+    const roomInfo = e.target.parentElement.parentElement.querySelector('.selected-room h4').textContent;
+    reservation.roomType = roomInfo;
+    console.log(reservation);
+    changeContent('guest-details-form-content');
+
+}
+document.querySelector('#guest-details-back-btn').addEventListener('click', (e) => fillRoomForm(e));
+
+function fillRoomForm(e) {
+    e.preventDefault();
+    changeContent('search-result-form-content');
+}
+
+document.querySelector('#guest-details-next-btn').addEventListener('click', (e) => getPersonalData(e));
+
+function getPersonalData(e) {
+    e.preventDefault();
+    const data = e.target.parentElement.parentElement;
+
+    const name = data.querySelector('#name').value;
+    const phone = data.querySelector('#phone-number').value;
+    const email = data.querySelector('#email').value;
+
+    if (name != '' && phone != '' && email != '') {
+        reservation.name = name;
+        reservation.phone = phone;
+        reservation.email = email;
+        console.log(reservation);
+        changeContent('confirm-reservation-content');
+        fillConfirmReservationData(reservation);
+    }
+}
+
+function fillConfirmReservationData(customReservation) {
+    document.querySelector('.confirm-reservation #guest-name').textContent = `Name: ${customReservation.name}`;
+    document.querySelector('.confirm-reservation #guest-phone').textContent = `Phone Number: ${customReservation.phone}`;
+    document.querySelector('.confirm-reservation #guest-email').textContent = `Email: ${customReservation.email}`;
+    document.querySelector('.confirm-reservation #guest-room-type').textContent = `Room Type: ${customReservation.roomType}`;
+    document.querySelector('.confirm-reservation #guest-data-in').textContent = `Date-in: ${customReservation.startDate}`;
+    document.querySelector('.confirm-reservation #guest-data-out').textContent = `Date-out: ${customReservation.endDate}`;
+}
+document.querySelector('#confirm-back-btn').addEventListener('click', (e) => getBackToPersonalData(e));
+
+function getBackToPersonalData(e) {
+    e.preventDefault();
+    changeContent('guest-details-form-content');
+}
+
+
+document.querySelector('#confirm-reservation').addEventListener('click', (e) => showThanksPage(e));
+
+function showThanksPage(e) {
+    e.preventDefault();
+    changeContent('thank-you-content');
+}
