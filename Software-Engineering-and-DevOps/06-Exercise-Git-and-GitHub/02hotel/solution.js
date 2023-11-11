7ade4aab0b1c9154f3ff4ddb9b1a5e2cb338e79a
Yanko Rashkov <thewhitedead@abv.bg>
2023-11-11 23:11:19 +0200

    Added base files for 02Hotel

---
+++ Software-Engineering-and-DevOps/06-Exercise-Git-and-GitHub/02hotel/solution.js
@@ -0,0 +1,23 @@
+let reservation =
+{
+    startDate: null,
+    endDate: null,
+    guestsCount: 0,
+    roomType: null,
+    name: null,
+    phone: null,
+    email: null
+}
+
+function changeContent(className) {
+    document.querySelectorAll('.custom-form').forEach(div => div.classList.add('hidden'));
+    if( document.querySelector(`.${className}`) != null){
+    document.querySelector(`.${className}`).classList.remove('hidden');
+    }
+}
+
+document.querySelector('#new-reservation').addEventListener('click', (e) => cleanData(e));
+
+function cleanData(e) {
+    changeContent('search-form-content');
+}
