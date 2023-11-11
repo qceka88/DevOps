1920a853c6cb91f3ae2bc0bb45be6a2194471c14
Yanko Rashkov <thewhitedead@abv.bg>
2023-11-11 22:01:28 +0200

    Test

--- Software-Engineering-and-DevOps/06-Exercise-Git-and-GitHub/01towns/towns.js
+++ Software-Engineering-and-DevOps/06-Exercise-Git-and-GitHub/01towns/towns.js
@@ -1,15 +1,8 @@
-$(document).ready(function() {
-	$('#btnDelete').click(deleteTown)
+$(document).ready(function () {
+    $('#btnDelete').click(deleteTown);
+    $('#btnAdd').click(addTown);
+    $('#btnShuffle').click(shuffleTowns);
 });
-$(document).ready(function() {
-	$('#btnShuffle').click(shuffleTowns);
-});
-
-
-$(document).ready(function() {
-	$('#btnAdd').click(addTown);
-});
-
 
 function deleteTown() {
     let townName = $('#townName').val();
@@ -27,31 +20,33 @@ function deleteTown() {
         showMessage(townName + " not found.");
 }
 
-function showMessage(msg) {
-    $('#result').text(msg).css("display", "block");
-    setTimeout(function () {
-        $('#result').hide('blind', {}, 500);
-    }, 3000);
-
 function addTown() {
     let townName = $('#townNameForAdd').val();
     $('#townNameForAdd').val('');
     $('#towns').append($('<option>').text(townName));
     $('#result').text(townName + " added.");
+}
+
+function showMessage(msg) {
+    $('#result').text(msg).css("display", "block");
+    setTimeout(function () {
+        $('#result').hide('blind', {}, 500);
+    }, 3000);
+}
 
 function shuffleTowns() {
-	let towns = $('#towns option').toArray();
-	$('#towns').empty();
-	shuffleArray(towns);
-	$('#towns').append(towns);
-	$('#result').text("Towns shuffled.");
+    let towns = $('#towns option').toArray();
+    $('#towns').empty();
+    shuffleArray(towns);
+    $('#towns').append(towns);
+    $('#result').text("Towns shuffled.");
 
-	function shuffleArray(array) {
-		for (var i = array.length - 1; i > 0; i--) {
-			var j = Math.floor(Math.random() * (i + 1));
-			var oldElement = array[i];
-			array[i] = array[j];
-			array[j] = oldElement;
-		}
-	}
+    function shuffleArray(array) {
+        for (var i = array.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var oldElement = array[i];
+            array[i] = array[j];
+            array[j] = oldElement;
+        }
+    }
 }
