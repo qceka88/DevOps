bad5268aa151d5a3b6681bf52d5996ecc206c536
Yanko Rashkov <thewhitedead@abv.bg>
2023-12-15 20:48:44 +0200

    Finished the Workshop. Added files for solution.
    docker-compose.yml  is only to build WebApp locally to check  functionality.

---
+++ Software-Engineering-and-DevOps/18-Workshop-DevOps-Project/terminal_commands.sh
@@ -0,0 +1,8 @@
+docker-compose up -d --build #To check it locally
+git init #To create GitHub Repository
+git add . #To add files
+git commit -m "Create Repository" #Add comments
+git push # Push repo  in 'main' branch
+
+
+
